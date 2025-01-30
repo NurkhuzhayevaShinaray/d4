9073406f26571cec867f4e9c297d59149aad4c95
torekhan1337 <torekhan1337@gmail.com>
2025-01-30 13:09:16 +0500

    Delete IReviewRepository.java

--- IReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package repositories.interfaces;
-
-import models.Review;
-import java.util.List;
-
-public interface IReviewRepository {
-    boolean addReview(Review review);
-    List<Review> getReviewsByUser(int userId);
-}
