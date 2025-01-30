1f554255275c955bc2632aa3dcbc9a109bb71a76
torekhan1337 <torekhan1337@gmail.com>
2025-01-30 13:13:38 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,23 @@
 import controllers.UserController;
-import controllers.IUserController;
+import controllers.ReviewController;
 import data.PostgresDB;
 import data.interfaces.IDB;
 import repositories.UserRepository;
+import repositories.ReviewRepository;
 import repositories.interfaces.IUserRepository;
+
+import application.MyApplication; // Импортируем MyApplication
 
 public class Main {
     public static void main(String[] args) {
         IDB db = new PostgresDB("jdbc:postgresql://localhost:5432/", "postgres", "Shin127ay");
-        IUserRepository repo = new UserRepository(db);
-        IUserController controller = new UserController(repo);
-        MyApplication app = new MyApplication(controller);
+        IUserRepository userRepo = new UserRepository(db);
+        UserController userController = new UserController(userRepo);
+
+        ReviewRepository reviewRepo = new ReviewRepository(db);
+        ReviewController reviewController = new ReviewController(reviewRepo);
+
+        MyApplication app = new MyApplication(userController, reviewController);
         app.start();
 
         db.close();
