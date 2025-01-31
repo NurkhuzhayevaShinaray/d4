c2eb477ffda1617fb0329e3bbe9095f7c82402f5
torekhan1337 <torekhan1337@gmail.com>
2025-01-31 05:10:40 +0500

    Update MyApplication.java

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,31 +1,28 @@
-import controllers.interfaces.IUserController;
-import controllers.ReviewController;
+package application;
+
+import controllers.UserController;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MyApplication {
-    private final IUserController userController;
-    private final ReviewController reviewController;
+    private final UserController userController;
     private final Scanner scanner = new Scanner(System.in);
 
-    public MyApplication(IUserController userController, ReviewController reviewController) {
+    public MyApplication(UserController userController) {
         this.userController = userController;
-        this.reviewController = reviewController;
     }
 
     private void mainMenu() {
         System.out.println();
-        System.out.println("Welcome to My Application");
+        System.out.println("Welcome to User Management");
         System.out.println("Select one of the following options:");
         System.out.println("1. Get all users");
         System.out.println("2. Get user by id");
         System.out.println("3. Create new user");
-        System.out.println("4. Add review");
-        System.out.println("5. Get reviews by user");
         System.out.println("0. Exit");
         System.out.println();
-        System.out.print("Select an option (0-5): ");
+        System.out.print("Select an option (0-3): ");
     }
 
     public void start() {
@@ -38,16 +35,15 @@ public class MyApplication {
                     case 1 -> getAllUsersMenu();
                     case 2 -> getUserByIdMenu();
                     case 3 -> createUserMenu();
-                    case 4 -> addReviewMenu();
-                    case 5 -> getReviewsByUserMenu();
-                    default -> {
-                        System.out.println("Exiting...");
+                    case 0 -> {
+                        System.out.println("Exiting User Management...");
                         return;
                     }
+                    default -> System.out.println("Invalid option. Please try again.");
                 }
             } catch (InputMismatchException e) {
                 System.out.println("Please enter a valid option! " + e);
-                scanner.nextLine(); // Ignore incorrect input
+                scanner.nextLine(); // Ignore invalid input
             } catch (Exception e) {
                 System.out.println("Error: " + e.getMessage());
             }
@@ -77,27 +73,6 @@ public class MyApplication {
 
     private void getAllUsersMenu() {
         String response = userController.getAllUsers();
-        System.out.println(response);
-    }
-
-    private void addReviewMenu() {
-        System.out.print("Enter user ID: ");
-        int userId = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Enter comment: ");
-        String comment = scanner.nextLine();
-        System.out.print("Enter rating (1-5): ");
-        int rating = scanner.nextInt();
-
-        String response = reviewController.addReview(userId, comment, rating);
-        System.out.println(response);
-    }
-
-    private void getReviewsByUserMenu() {
-        System.out.print("Enter user ID: ");
-        int userId = scanner.nextInt();
-
-        String response = reviewController.getReviewsByUser(userId);
         System.out.println(response);
     }
 }
